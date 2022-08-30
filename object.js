7c4d597cd46f963b504752f6513414f84b0c7a79
samro01 <srozario22@gmail.com>
2022-08-30 17:43:39 +0200

    first commit

---
+++ object.js
@@ -0,0 +1,36 @@
+// ways to creat object
+// 
+/*1.object literel syntax(imp)
+
+const person = {
+    firstName : "john",
+    lastName : "doe",
+}
+
+2.Object Constructor
+ const person = new Object();
+ person.firstName : "john";
+ person.lastName : "doe";
+
+ 3.Constructor Function
+function Person(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+// 4.ES6 classes:(imp)
+class person{
+    constructor(firstName,lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+const person1 = new Person("john", "Doe");
+
+5.Object.creat()
+const person1 = {
+    name:"John Doe"
+    const person2 = Object.creat(person1);
+    console.log(person2);
+}
+*/
